cac57ae7c03d2a9ff0b4f73de64ac39f2f9e96c7
Sebastian Pritz <S2210595013@fhooe.at>
2024-01-20 13:27:51 +0100

    Status: Canary deployments, trying with tutorial page

--- app/app.py
+++ app/app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"<p>Hello, World! I am at {socket.gethostname()}</p>"
+    return f"""
+    <html>
+        <head>
+            <title>Canary Release</title>
+        </head>
+        <body style="background-color: lightblue">
+            <p>Hello, World! I am at {socket.gethostname()}</p>
+        </body>
+    </html>"""
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
